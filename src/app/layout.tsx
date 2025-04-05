bbfb83fd37fcf46d3d4829c2c115f0d4639a8d95
italocunhas <italocunhasilva1@gmail.com>
2025-04-05 19:14:16 -0300

    feat: atualiza favicon para mostrar a letra G

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Header } from '../components/header';
-import { CartProvider } from '../contexts/CartContext';
-import { Providers } from "./providers";
+import { Header } from '@/components/header';
+import { CartProvider } from '@/contexts/CartContext';
+import { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Açaí Goitacá',
-  description: 'Monte seu açaí personalizado',
+  description: 'O melhor açaí de Bangu!',
   metadataBase: new URL('https://acaigoitaca.vercel.app'),
   icons: {
-    icon: '/favicon.ico',
+    icon: '/favicon.svg',
   },
 };
 
@@ -24,12 +24,11 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
-        <Providers>
-          <CartProvider>
-            <Header />
-            {children}
-          </CartProvider>
-        </Providers>
+        <CartProvider>
+          <Header />
+          {children}
+          <Toaster position="bottom-right" />
+        </CartProvider>
       </body>
     </html>
   );
